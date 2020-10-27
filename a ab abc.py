3e1ea94fb113ed6dc0cc3eb0f351429fcfe39547
yashlearnpython <73551736+yashlearnpython@users.noreply.github.com>
2020-10-27 23:36:32 +0530

    Add files via upload

---
+++ a ab abc.py
@@ -0,0 +1,7 @@
+for i in range(1 ,6):
+    x = 65
+    for j in range(1 , i+1 ):
+        print(chr(x), end="")
+        x += 1
+
+    print()
---
+++ add two number.py
@@ -0,0 +1,4 @@
+num1 =int(input("enter 1st number = "))
+num2 =int(input("enter 2nd number = "))
+result = num1+ num2
+print (num1," + ",num2," = ",result)
